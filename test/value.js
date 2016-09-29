b88a2233abf5bb5790c9b9393ef9c911830530d2
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-09-29 07:34:50 -0700

    Added verification that this works with deeply nested objects.

--- test/value.js
+++ test/value.js
@@ -22,4 +22,21 @@ describe('ValueObject value', function(it) {
         object.key.secondKey = 'roar';
         assert.ok(object.key.secondKey.valueOf() === 'value');
     });
+
+    it('cannot be changed as a deeply nested object', function(assert) {
+        var valueObject = self.define('ValueObject', { validate: () => true });
+        var object = new valueObject({
+            key1: {
+                key2: {
+                    key3: {
+                        key4: {
+                            key5: 'value'
+                        }
+                    }
+                }
+            }
+        });
+        object.key1.key2.key3.key4.key5 = 'roar';
+        assert.ok(object.key1.key2.key3.key4.key5.valueOf() === 'value');
+    });
 });
