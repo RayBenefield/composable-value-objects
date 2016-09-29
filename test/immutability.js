925626186303d7fd0122db1d97b56047a57385c6
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-09-29 16:06:33 -0700

    Changed the wording for tests around immutability.

--- test/immutability.js
+++ test/immutability.js
@@ -1,29 +1,29 @@
 var describe = require('tape-bdd');
 var self = require('../src/valueObject');
 
-describe('ValueObject value', function(it) {
-    it('cannot be changed as a primitive', function(assert) {
+describe('ValueObject immutability', function(it) {
+    it('holds true for a primitive', function(assert) {
         var valueObject = self.define('ValueObject', { validate: () => true });
         var object = new valueObject('test');
         object.value = 'roar';
         assert.ok(object.valueOf() === 'test');
     });
 
-    it('cannot be changed as an object', function(assert) {
+    it('holds true for an object', function(assert) {
         var valueObject = self.define('ValueObject', { validate: () => true });
         var object = new valueObject({ key: 'value' });
         object.key = 'roar';
         assert.ok(object.key.valueOf() === 'value');
     });
 
-    it('cannot be changed as a nested object', function(assert) {
+    it('holds true for a nested object', function(assert) {
         var valueObject = self.define('ValueObject', { validate: () => true });
         var object = new valueObject({ key: { secondKey: 'value' } });
         object.key.secondKey = 'roar';
         assert.ok(object.key.secondKey.valueOf() === 'value');
     });
 
-    it('cannot be changed as a deeply nested object', function(assert) {
+    it('holds true for a deeply nested object', function(assert) {
         var valueObject = self.define('ValueObject', { validate: () => true });
         var object = new valueObject({
             key1: {
