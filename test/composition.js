508f3d9aa5ff5e78e8a793a282655d8bfddcf9f0
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-10-03 06:23:40 -0700

    Added a check for multiple composite object creation.

--- test/composition.js
+++ test/composition.js
@@ -20,4 +20,32 @@ describe('ValueObject composition', function(it) {
         var result = new valueObject('composite.test');
         assert.ok(result.composite.valueOf() === 'test');
     });
+
+    it('passes parsed values to multiple composite objects', function(assert) {
+        var composite1 = self.define('Composite #1', {
+            validate: (object) => object.value === 'test1'
+        });
+
+        var composite2 = self.define('Composite #2', {
+            validate: (object) => object.value === 'test2'
+        });
+
+        var valueObject = self.define('Value Object', {
+            validate: () => true,
+            composites: {
+                "compositeOne": composite1,
+                "compositeTwo": composite2,
+            },
+            preParsers: {
+                "compositeOne": (object) => object.value.split('.')[0],
+                "compositeTwo": (object) => object.value.split('.')[1]
+            }
+        });
+
+        var result = new valueObject('test1.test2');
+        assert.ok(
+            result.compositeOne.valueOf() === 'test1'
+            && result.compositeTwo.valueOf() === 'test2'
+        );
+    });
 });
