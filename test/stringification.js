2263db86429c55d73f3588c3d151aa31a7a537a8
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-10-03 07:32:58 -0700

    Added a check for stringifying a regular value.

--- test/stringification.js
+++ test/stringification.js
@@ -2,6 +2,12 @@ var describe = require('tape-bdd');
 var self = require('src/value-object');
 
 describe('ValueObject stringification', function(it) {
+    it('returns an stringified value', function(assert) {
+        var valueObject = self.define('ValueObject', { validate: () => true });
+        var object = new valueObject('test');
+        assert.ok(object.toString() === '"test"');
+    });
+
     it('returns an stringified object', function(assert) {
         var valueObject = self.define('ValueObject', { validate: () => true });
         var object = new valueObject({ test: 'string' });
