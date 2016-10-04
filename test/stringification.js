cb6b8fbbc727391dba23534420bfdb24bebbbfb3
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-10-04 02:46:17 -0700

    Added a test case for stringifying an object with multiple composites using an object as a value.

--- test/stringification.js
+++ test/stringification.js
@@ -2,19 +2,19 @@ var describe = require('tape-bdd');
 var self = require('src/value-object');
 
 describe('ValueObject stringification', function(it) {
-    it('returns an stringified value', function(assert) {
+    it('returns a stringified value', function(assert) {
         var valueObject = self.define('ValueObject', { validate: () => true });
         var object = new valueObject('test');
         assert.ok(object.toString() === '"test"');
     });
 
-    it('returns an stringified object', function(assert) {
+    it('returns a stringified object', function(assert) {
         var valueObject = self.define('ValueObject', { validate: () => true });
         var object = new valueObject({ test: 'string' });
         assert.ok(object.toString() === '{"test":"string"}');
     });
 
-    it('returns an stringified object with a parsed value', function(assert) {
+    it('returns a stringified object with a parsed value', function(assert) {
         var valueObject = self.define('ValueObject', {
             validate: () => true,
             preParsers: {
@@ -25,7 +25,7 @@ describe('ValueObject stringification', function(it) {
         assert.ok(object.toString() === '{"test":"roar"}');
     });
 
-    it('returns an stringified object with a composite object', function(assert) {
+    it('returns a stringified object with a composite object', function(assert) {
         var composite = self.define('Composite', {
             validate: (object) => object.value === 'test'
         });
@@ -44,7 +44,7 @@ describe('ValueObject stringification', function(it) {
         assert.ok(result.toString() === '{"composite":"test"}');
     });
 
-    it('returns an stringified object with multiple composite objects', function(assert) {
+    it('returns a stringified object with multiple composite objects', function(assert) {
         var composite1 = self.define('Composite #1', {
             validate: (object) => object.value === 'test1'
         });
@@ -69,7 +69,32 @@ describe('ValueObject stringification', function(it) {
         assert.ok(result.toString() === '{"compositeOne":"test1","compositeTwo":"test2"}');
     });
 
-    it('returns an stringified object with nested composite objects', function(assert) {
+    it('returns a stringified object with multiple composite objects using an obect as a value', function(assert) {
+        var composite1 = self.define('Composite #1', {
+            validate: (object) => object.value === 'test1'
+        });
+
+        var composite2 = self.define('Composite #2', {
+            validate: (object) => object.value === 'test2'
+        });
+
+        var valueObject = self.define('Value Object', {
+            validate: () => true,
+            composites: {
+                "compositeOne": composite1,
+                "compositeTwo": composite2,
+            },
+            preParsers: {
+                "compositeOne": (object) => object.value.property.split('.')[0],
+                "compositeTwo": (object) => object.value.property.split('.')[1]
+            }
+        });
+
+        var result = new valueObject({ property: 'test1.test2' });
+        assert.ok(result.toString() === '{"property":"test1.test2","compositeOne":"test1","compositeTwo":"test2"}');
+    });
+
+    it('returns a stringified object with nested composite objects', function(assert) {
         var compositeDeep = self.define('Composite Deep', {
             validate: (object) => object.original === 'test2'
         });
