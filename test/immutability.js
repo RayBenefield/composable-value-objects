6a0a4e30c0bf9f01e54b4bd38021d7bab01ddba9
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-10-05 16:50:43 -0700

    Fixed the immutability test errors and added some changes to the eslintrc.

--- test/immutability.js
+++ test/immutability.js
@@ -1,118 +1,118 @@
-var describe = require('tape-bdd');
-var self = require('src/value-object');
+import describe from 'tape-bdd';
+import Self from 'src/value-object';
 
-describe('ValueObject immutability', function(it) {
-    it('holds true for a primitive', function(assert) {
-        var valueObject = self.define('ValueObject', { validate: () => true });
-        var object = new valueObject('test');
+describe('ValueObject immutability', (it) => {
+    it('holds true for a primitive', (assert) => {
+        const ValueObject = Self.define('ValueObject', { validate: () => true });
+        const object = new ValueObject('test');
         assert.throws(() => object.value = 'roar');
     });
 
-    it('holds true for an object', function(assert) {
-        var valueObject = self.define('ValueObject', { validate: () => true });
-        var object = new valueObject({ key: 'value' });
+    it('holds true for an object', (assert) => {
+        const ValueObject = Self.define('ValueObject', { validate: () => true });
+        const object = new ValueObject({ key: 'value' });
         assert.throws(() => object.key = 'roar');
     });
 
-    it('holds true for a nested object', function(assert) {
-        var valueObject = self.define('ValueObject', { validate: () => true });
-        var object = new valueObject({ key: { secondKey: 'value' } });
+    it('holds true for a nested object', (assert) => {
+        const ValueObject = Self.define('ValueObject', { validate: () => true });
+        const object = new ValueObject({ key: { secondKey: 'value' } });
         assert.throws(() => object.key.secondKey = 'roar');
     });
 
-    it('holds true for a deeply nested object', function(assert) {
-        var valueObject = self.define('ValueObject', { validate: () => true });
-        var object = new valueObject({
+    it('holds true for a deeply nested object', (assert) => {
+        const ValueObject = Self.define('ValueObject', { validate: () => true });
+        const object = new ValueObject({
             key1: {
                 key2: {
                     key3: {
                         key4: {
-                            key5: 'value'
-                        }
-                    }
-                }
-            }
+                            key5: 'value',
+                        },
+                    },
+                },
+            },
         });
         assert.throws(() => object.key1.key2.key3.key4.key5 = 'roar');
     });
 
-    it('holds true for a parsed properties', function(assert) {
-        var valueObject = self.define('ValueObject', {
+    it('holds true for a parsed properties', (assert) => {
+        const ValueObject = Self.define('ValueObject', {
             validate: () => true,
             preParsers: {
-                parsed: () => 'test'
-            }
+                parsed: () => 'test',
+            },
         });
-        var object = new valueObject();
+        const object = new ValueObject();
         assert.throws(() => object.parsed = 'roar');
     });
 
-    it('holds true for a nested parsed properties', function(assert) {
-        var valueObject = self.define('ValueObject', {
+    it('holds true for a nested parsed properties', (assert) => {
+        const ValueObject = Self.define('ValueObject', {
             validate: () => true,
             preParsers: {
-                'parsed.nested': (valueObject) => valueObject.value.split('.')[1],
-            }
+                'parsed.nested': valueObject => valueObject.value.split('.')[1],
+            },
         });
-        var object = new valueObject('test.parsed');
+        const object = new ValueObject('test.parsed');
         assert.throws(() => object.parsed.nested = 'roar');
     });
 
-    it('holds true for deeply nested parsed properties', function(assert) {
-        var valueObject = self.define('ValueObject', {
+    it('holds true for deeply nested parsed properties', (assert) => {
+        const ValueObject = Self.define('ValueObject', {
             validate: () => true,
             preParsers: {
-                'parsed.nested.again.and.again': (valueObject) => valueObject.value.split('.')[1],
-            }
+                'parsed.nested.again.and.again': valueObject => valueObject.value.split('.')[1],
+            },
         });
-        var object = new valueObject('test.parsed');
+        const object = new ValueObject('test.parsed');
         assert.throws(() => object.parsed.nested.again.and.again = 'roar');
     });
 
-    it('stops post parsers from editing the value', function(assert) {
-        var valueObject = self.define('ValueObject', {
+    it('stops post parsers from editing the value', (assert) => {
+        const ValueObject = Self.define('ValueObject', {
             validate: () => true,
             postParsers: {
-                value: () => 'changed'
-            }
+                value: () => 'changed',
+            },
         });
-        var object = new valueObject('test');
+        const object = new ValueObject('test');
         assert.ok(object.value !== 'changed');
     });
 
-    it('stops post parsers from editing properties from pre parsers', function(assert) {
-        var valueObject = self.define('ValueObject', {
+    it('stops post parsers from editing properties from pre parsers', (assert) => {
+        const ValueObject = Self.define('ValueObject', {
             validate: () => true,
             preParsers: {
-                property: () => 'added'
+                property: () => 'added',
             },
             postParsers: {
-                anotherProperty: (valueObject) => valueObject.property = 'changed'
-            }
+                anotherProperty: valueObject => valueObject.property = 'changed',
+            },
         });
-        assert.throws(() => new valueObject('test'));
+        assert.throws(() => new ValueObject('test'));
     });
 
-    it('holds true for post parsed properties', function(assert) {
-        var valueObject = self.define('ValueObject', {
-            validate: () => true,
-            postParsers: {
-                property: () => 'added'
-            }
-        });
-        var object = new valueObject('test');
-        assert.throws(() => object.property = 'changed');
-    });
-
-    it('still allows post parsed properties to be modified by other post parsers', function(assert) {
-        var valueObject = self.define('ValueObject', {
+    it('holds true for post parsed properties', (assert) => {
+        const ValueObject = Self.define('ValueObject', {
             validate: () => true,
             postParsers: {
                 property: () => 'added',
-                anotherProperty: (valueObject) => valueObject.property = 'changed'
-            }
+            },
         });
-        var object = new valueObject('test');
+        const object = new ValueObject('test');
+        assert.throws(() => object.property = 'changed');
+    });
+
+    it('still allows post parsed properties to be modified by other post parsers', (assert) => {
+        const ValueObject = Self.define('ValueObject', {
+            validate: () => true,
+            postParsers: {
+                property: () => 'added',
+                anotherProperty: valueObject => valueObject.property = 'changed',
+            },
+        });
+        const object = new ValueObject('test');
         assert.ok(object.property === 'changed');
     });
 });
