c58473374a65d80c86b6a53b760f4ec562406884
Raymond Benefield <Raymond.Benefield@gmail.com>
2016-10-08 10:53:18 -0700

    Added a check to make sure nested values of different objects are the equal.

--- test/equality.js
+++ test/equality.js
@@ -17,4 +17,12 @@ describe('ValueObject equality', (it) => {
         assert.ok(object1 === object2);
         Self.clearDatabase();
     });
+
+    it('the nested values of objects are truly equal', (assert) => {
+        const ValueObject = Self.define('Value Object', { validate: () => true });
+        const object1 = new ValueObject({ test: 'test' });
+        const object2 = new ValueObject({ test: 'test' });
+        assert.ok(object1.test === object2.test);
+        Self.clearDatabase();
+    });
 });
